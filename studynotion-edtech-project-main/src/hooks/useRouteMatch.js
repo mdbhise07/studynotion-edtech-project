6b66711ad2fd65d0129f24f43e646c42fe4147cc
mdbhise07 <md.bhise3217@gmail.com>
2024-11-19 17:22:14 +0530

    Initial commit

---
+++ studynotion-edtech-project-main/src/hooks/useRouteMatch.js
@@ -0,0 +1,5 @@
+import { useLocation, matchPath } from "react-router-dom";
+export default function useRouteMatch(path) {
+  const location = useLocation();
+  return matchPath(location.pathname, { path });
+}
